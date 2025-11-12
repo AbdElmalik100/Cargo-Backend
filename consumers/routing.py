ebd07589a334d819de19f286550506219bfe39e1
AbdElmalik100 <paydaychains100@gmail.com>
2025-11-12 14:19:54 +0200

    updates in routing

--- consumers/routing.py
+++ consumers/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from .shipments import ShipmentsConsumer
 
 websocket_urlpatterns = [
-    re_path(r'^ws/shipments/stats/$', ShipmentsConsumer.as_asgi()),
+    re_path(r'^ws/shipments/$', ShipmentsConsumer.as_asgi()),
 ]
